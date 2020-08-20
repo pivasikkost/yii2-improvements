a0d8b78ff5f2ca67b7a1531dfbd519000e694a7a
pivasikkost <pivasikkost@gmail.com>
2020-08-20 17:09:56 +0300

    Update ArraySearchModelTrait.php

--- models/ArraySearchModelTrait.php
+++ models/ArraySearchModelTrait.php
@@ -20,15 +20,41 @@ use yii\data\ArrayDataProvider;
 trait ArraySearchModelTrait
 {
     /**
+     * (traits cannot contain static properties, but you can set them in your class)
+     */
+    
+    /**
+     * @var array The columns to be selected from the table in the desired order
+     */
+    //public static $columns_for_select = ['ASOURCE', 'TYPE_PRODUCT'];
+    
+    /**
+     * @var array Attribute names for which strict comparison should be applied 
+     * when filtering in ArraySearchModelTrait filter() method
+     */
+    //public static $strict_comparison = ['ASOURCE' => true, 'TYPE_PRODUCT' => true];
+    
+    /**
+     * @var bool Flag whether leading zeros should be added to numerical values
+     */
+    //public static $add_leading_zeros = true;
+    
+    /**
+     * @var int The time, in seconds, for which you need to cache data
+     * in the ArraySearchModelTrait getAllData() method
+     */
+    //public static $cache_duration = 600; //10min
+    
+    /**
      * Creates data provider instance with search query applied
      *
      * @param array $params
      * @return ArrayDataProvider
      */
-    public function search($params)
+    public function search($params, $from_cache = null)
     {
         $query = parent::find()->asArray();
-        $allModels = $this->getAllModels($query);
+        $allModels = $this->getAllModels($query, $from_cache);
         
         $this->load($params);
         if ($this->validate()) {
@@ -61,13 +87,25 @@ trait ArraySearchModelTrait
         $duration = static::$cacheDuration ?? 600; //10min
         $from_cache = $from_cache ?? Yii::$app->request->isAjax;
         
+        if (isset(static::$columns_for_select)) {
+            $query->select(static::$columns_for_select);
+        }
+        
         if ($from_cache) {
             return $cache->getOrSet($key, function () use ($query) {
-                return $query->all();
+                if (!empty(static::$add_leading_zeros)) {
+                    return static::addLeadingZeros($query->all());
+                } else {
+                    return $query->all();
+                }
             }, $duration);
         } else {
             $cache->delete($key);
-            $allModels = $query->all();
+            if (!empty(static::$add_leading_zeros)) {
+                $allModels = static::addLeadingZeros($query->all());
+            } else {
+                $allModels = $query->all();
+            }
             $cache->set($key, $allModels, $duration);
             return $allModels;
         }
@@ -84,19 +122,56 @@ trait ArraySearchModelTrait
         foreach($data as $rowIndex => $row) {
             foreach($this->getAttributes() as $key => $value) {
                 // unset if filter is set, but doesn't match
-                if(array_key_exists($key, $row) && trim($value) !== '') {
-                    if (static::getTableSchema()->columns[$key]->dbType == 'NUMBER'
-                        || isset(static::$strict_comparison[$key])
-                    ) {
-                        if ($row[$key] != $value) {
+                if (array_key_exists($key, $row)) {
+                    if (is_array($value)) { //multiselect|checkbox
+                        $unset = true;
+                        foreach ($value as $value_item) {
+                            if ($row[$key] === $value_item) {
+                                $unset = false;
+                                break;
+                            }
+                        }
+                        if ($unset) {
                             unset($data[$rowIndex]);
                         }
-                    } elseif (mb_stripos($row[$key], $value) === false) {
-                        unset($data[$rowIndex]);
+                    } elseif (trim($value) !== '') { //other inputs
+                        if (static::getTableSchema()->columns[$key]->dbType == 'NUMBER') { // '.' = ','
+                            if (CustomStringHelper::addLeadingZeroAndRound($row[$key]) !== CustomStringHelper::addLeadingZeroAndRound($value)) {
+                                unset($data[$rowIndex]);
+                            }
+                        } elseif (isset(static::$strict_comparison[$key])) {
+                            if ($row[$key] !== $value) {
+                                unset($data[$rowIndex]);
+                            }
+                        } elseif (mb_stripos($row[$key], $value) === false) {
+                            unset($data[$rowIndex]);
+                        }
                     }
                 }
             }
         }
+        
+        return $data;
+    }
+    
+    /**
+     * Rounds numerical values and adds leading zeros
+     *
+     * @param array $data Values without leading zeros
+     * @return array Values with leading zeros
+     */
+    public function addLeadingZeros(array $data): array
+    {
+        foreach($data as &$row) {
+            foreach(static::getTableSchema()->columns as $key => $value) {
+                if (array_search($key, static::$columns_for_select) !== false
+                    && $value->dbType === 'NUMBER'
+                ) {
+                    $row[$key] = CustomStringHelper::addLeadingZeroAndRound($row[$key], 9, ',');
+                }
+            }
+        }
+        unset($row);
         
         return $data;
     }
